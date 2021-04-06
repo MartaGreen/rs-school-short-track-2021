b845346e79d0ae7e9643aef803782d706cd21b5f
martagreen <portnykhmaria@gmail.com>
2021-04-06 21:00:00 +0400

    feat: did the mac adress task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,17 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const lettersArr = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const arr = String(n).split('-');
+  if (arr.length !== 6) return false;
+  const answ = arr.every((elem) => (
+    elem.length === 2
+      && ((Number.isNaN(elem[0]) && lettersArr.indexOf(elem[0]) !== -1)
+      || !Number.isNaN(elem[0])) && ((Number.isNaN(elem[1]) && lettersArr.indexOf(elem[1]) !== -1)
+      || !Number.isNaN(elem[1]))
+  ));
+  return answ;
 }
 
 module.exports = isMAC48Address;
