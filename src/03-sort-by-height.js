3e341534de4aee0aac9e5f98557abd0bb1f926a6
martagreen <portnykhmaria@gmail.com>
2021-04-07 14:10:34 +0400

    feat: comlete sort by height task

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,17 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const array = arr;
+  const newArr = array.filter((elem) => elem !== -1).sort();
+  let counter = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== -1) {
+      array[i] = newArr[counter];
+      counter += 1;
+    }
+  }
+  return array;
 }
 
 module.exports = sortByHeight;
